0addd21e56024800c18159915bb2b7fd0d80bf5e
shivam9pal <shivampaul9794@gmail.com>
2025-04-25 12:07:29 +0530

    created user login

--- app/src/main/java/org/example/entities/User.java
+++ app/src/main/java/org/example/entities/User.java
@@ -4,8 +4,39 @@ import java.util.List;
 
 public class User {
     private String name;
+
     private String password;
-    private String hashPassword;
+
+    private String hashedPassword;
+
     private List<Ticket> tickets;
+
     private String userId;
+
+    public User(String name,String password, String hashedPassword ,List<Ticket> tickets ,String userId){
+        this.name=name;
+        this.password=password;
+        this.hashedPassword=hashedPassword;
+        this.tickets=tickets;
+        this.userId=userId;
+    }
+    //default constructor for without value
+    public User(){};
+
+    public String getName(){
+        return name;
+    }
+
+    public String getPassword(){
+        return password;
+    }
+    public String getHashedPassword(){
+        return hashedPassword;
+    }
+    public List<Ticket> getTicketsBooked() {
+        return getTicketsBooked();
+    }
+    public String getUserId(){
+        return userId;
+    }
 }
--- app/src/main/java/org/example/services/UserBookingService.java
+++ app/src/main/java/org/example/services/UserBookingService.java
@@ -1,10 +1,13 @@
 package org.example.services;
 
+import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.example.entities.User;
 
 import java.io.File;
+import java.io.IOException;
 import java.util.List;
+import java.util.Optional;
 
 public class UserBookingService {
     private User user;
@@ -13,11 +16,33 @@ public class UserBookingService {
 
     private ObjectMapper  objectMapper =new ObjectMapper();
 
-    private static final String USER_PATH="../localDB/user.json";
+    private static final String USER_PATH="app/src/main/java/example/localDB/user.json ";
 
-    public UserBookingService(User user){
+    public UserBookingService(User user)throws IOException
+    {
         this.user=user;
         File  users=new File(USER_PATH);
+        userList = objectMapper.readValue(users, new TypeReference<List<User>>() {});
 
     }
+
+    public Boolean loginUser(){
+        Optional<User> foundUser=userList.stream().filter(user1 -> {
+            return user1.getName().equals(user.getName()) && UserServiceUtil.checkPassword(user.getPassword(),user1)
+        }).findFirst();
+        return foundUser.isPresent();
+    }
+
+    public Boolean signUp(User user1){
+        try{
+            userList.add(user1);
+            saveUserListToFile();
+            return Boolean.TRUE;
+        }catch (IOException ex){
+            return Boolean.FALSE;
+        }
+
+
+    }
+
 }
