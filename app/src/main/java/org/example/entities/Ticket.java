2d12431ffc61f48e244f9e49a2f9a57a5fa876ae
shivam9pal <shivampaul9794@gmail.com>
2025-04-29 11:21:54 +0530

    updating main file

--- app/src/main/java/org/example/entities/Ticket.java
+++ app/src/main/java/org/example/entities/Ticket.java
@@ -15,4 +15,68 @@ public class Ticket {
 
     private Train train;
 
+    public Ticket(String ticketId,String userId, String source, String destination, Date dateOfTravel, Train train){
+        this.ticketId=ticketId;
+        this.userId=userId;
+        this.source=source;
+        this.destination=destination;
+        this.dateOfTravel=dateOfTravel;
+        this.train=train;
+    }
+
+    public String getTicketInfo(){
+        return String.format("TicketID :%s belongs to user %s  from %s to %s on %s",ticketId,userId,source,destination,dateOfTravel);
+    }
+
+    public String getTicketId() {
+        return ticketId;
+    }
+
+    public String getUserId() {
+        return userId;
+    }
+
+    public String getSource() {
+        return source;
+    }
+
+    public String getDestination() {
+        return destination;
+    }
+
+    public Date getDateOfTravel() {
+        return dateOfTravel;
+    }
+
+    public Train getTrain() {
+        return train;
+    }
+
+
+
+    public void setTicketId(String ticketId) {
+        this.ticketId = ticketId;
+    }
+
+    public void setUserId(String userId) {
+        this.userId = userId;
+    }
+
+    public void setSource(String source) {
+        this.source = source;
+    }
+
+    public void setDestination(String destination) {
+        this.destination = destination;
+    }
+
+    public void setDateOfTravel(Date dateOfTravel) {
+        this.dateOfTravel = dateOfTravel;
+    }
+
+    public void setTrain(Train train) {
+        this.train = train;
+    }
+
+
 }
--- app/src/main/java/org/example/services/UserBookingService.java
+++ app/src/main/java/org/example/services/UserBookingService.java
@@ -3,6 +3,7 @@ package org.example.services;
 import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.example.entities.User;
+import org.example.util.UserServiceUtil;
 
 import java.io.File;
 import java.io.IOException;
@@ -11,38 +12,57 @@ import java.util.Optional;
 
 public class UserBookingService {
     private User user;
-
     private List<User> userList;
 
-    private ObjectMapper  objectMapper =new ObjectMapper();
+    private ObjectMapper objectMapper = new ObjectMapper();
+    private static final String USER_PATH = "app/src/main/java/example/localDB/user.json";
 
-    private static final String USER_PATH="app/src/main/java/example/localDB/user.json ";
 
-    public UserBookingService(User user)throws IOException
-    {
-        this.user=user;
-        File  users=new File(USER_PATH);
+    public UserBookingService(User user) throws IOException {
+        this.user = user;
+        File users = new File(USER_PATH);
         userList = objectMapper.readValue(users, new TypeReference<List<User>>() {});
-
     }
 
-    public Boolean loginUser(){
-        Optional<User> foundUser=userList.stream().filter(user1 -> {
-            return user1.getName().equals(user.getName()) && UserServiceUtil.checkPassword(user.getPassword(),user1)
+
+    public UserBookingService() throws IOException {
+        this.userList = loadUser();
+    }
+
+    public List<User> loadUser() throws IOException {
+        File users = new File(USER_PATH);
+        return objectMapper.readValue(users, new TypeReference<List<User>>() {});
+    }
+
+    public Boolean loginUser() {
+        Optional<User> foundUser = userList.stream().filter(user1 -> {
+            return user1.getName().equals(user.getName()) &&
+                    UserServiceUtil.checkPassword(user.getPassword(), user1.getPassword());
         }).findFirst();
         return foundUser.isPresent();
     }
 
-    public Boolean signUp(User user1){
-        try{
+    public Boolean signUp(User user1) throws IOException {
+        try {
             userList.add(user1);
             saveUserListToFile();
             return Boolean.TRUE;
-        }catch (IOException ex){
+        } catch (IOException ex) {
             return Boolean.FALSE;
         }
-
-
     }
 
+    public void saveUserListToFile() throws IOException {
+        File userFile = new File(USER_PATH);
+        objectMapper.writeValue(userFile, userList);
+    }
+
+    public void fetchBooking() {
+        user.printTickets();
+    }
+
+    public Boolean cancelBooking(String ticketId) {
+
+        return Boolean.FALSE;
+    }
 }
